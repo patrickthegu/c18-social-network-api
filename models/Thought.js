783b6290a1b3c381d8cfdf13f5466c2b4dba6726
Patrick Gu <patrickthegu@gmail.com>
2022-09-28 16:25:03 +1000

    Models updated

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction')
+
+const thoughtSchema = new Schema({
+    thoughtContent: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => moment(timestamp).format('dddd Do MMM, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+})
+
+const Thought = model('Thought', thoughtSchema)
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,37 +1,43 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      // Regex to validate email address
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Email must match']
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types
+    }]
   },
   {
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false,
-  }
-);
+  },
+)
 
-// Create a virtual property `commentCount` that gets the amount of comments per user
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+})
 
-// Initialize our User model
-const User = model('user', userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
